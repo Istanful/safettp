f9fa988d97dbd5fdb13cf46f675f143da5109b94
Emric <w.e.w@live.se>
2018-08-29 10:39:30 +0200

    Add http methods to client

--- lib/safettp/client.rb
+++ lib/safettp/client.rb
@@ -6,6 +6,26 @@ class Safettp::Client
     @options_hash = options_hash
   end
 
+  def get(uri_suffix, query, options = { query: query }, &block)
+    perform(:get, uri_suffix, options, &block)
+  end
+
+  def post(uri_suffix, body, options = { body: body }, &block)
+    perform(:post, uri_suffix, options, &block)
+  end
+
+  def put(uri_suffix, body, options = { body: body }, &block)
+    perform(:put, uri_suffix, options, &block)
+  end
+
+  def patch(uri_suffix, body, options = { body: body }, &block)
+    perform(:patch, uri_suffix, options, &block)
+  end
+
+  def delete(uri_suffix, query, options = { query: query }, &block)
+    perform(:delete, uri_suffix, options, &block)
+  end
+
   def perform(*args, &block)
     response = perform_without_guard(*args)
     guard = Safettp::Guard.new(response)
@@ -13,9 +33,9 @@ class Safettp::Client
     guard.evaluate!
   end
 
-  def perform_without_guard(method, uri_suffix = '/', additional_options = {})
+  def perform_without_guard(method, uri_suffix = '/', options = {})
     url = "#{base_url}#{uri_suffix}"
-    Safettp::Request.new(url, options_hash.merge(additional_options))
+    Safettp::Request.new(url, options_hash.merge(options))
                     .perform(method)
   end
 end
